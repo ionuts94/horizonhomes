545b6d84cc043eb051acdeecd618fc830415244a
ionuts94 <ionut.s_94@yahoo.com>
2023-02-13 00:35:27 +0200

    WIP

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { Home } from './Home/Home';
+export { Profile } from './Profile/Profile';
+export { SignIn } from './SignIn/SignIn';
+export { SignUp } from './SignUp/SignUp';
+export { ForgotPassword } from './ForgotPassword/ForgotPassword';
+export { Offers } from './Offers/Offers';
